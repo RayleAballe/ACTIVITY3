af394934a0dfbe03e1eca841844fdaf2133b4a18
Student <Student@CLB1Unit46>
2024-02-14 14:31:38 +0800

    ACTIVITY 3

---
+++ src/act/pkg3/Act3.java
@@ -0,0 +1,19 @@
+package act.pkg3;
+
+
+public class Act3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double c = 21f;
+            double a = 1.8f;
+            double b = 32f;
+      
+      double result = c * a + b;
+        System.out.println(c*a+b);
+              
+    }
+    
+}
